03e7435e194f8ce3cf2135252dd1fdfce16fc998
Leanderp <leanderperez15@gmail.com>
2021-02-19 18:00:43 -0400

    crud funcional

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 
 # -------- FUNCIONES ----------#
 
+# -------- FUNCIONES - MENU - BBDD ----------#
 def conexionBBDD():
     miConexion = sqlite3.connect("Usuarios")
     miCursor = miConexion.cursor()
@@ -21,13 +22,104 @@ def conexionBBDD():
         messagebox.showinfo("BBDD","BBDD creada con exito")
     except:
        messagebox.showwarning("!Atencion!","La BBDD ya existe")
+    finally:
+        miConexion.close()
 
 def salirAplicacion():
     valor =  messagebox.askquestion("Salir", "¿Deseas salir de la aplicacion?")
 
     if valor =="yes":
         root.destroy()
+
+# -------- FUNCIONES - MENU - BORRAR ----------#
+
+def limpiarCampos():
+    miId.set("")
+    miNombre.set("")
+    miApellido.set("")
+    miPass.set("")
+    miDireccion.set("")
+    textoComentario.delete(1.0,END)
+
+# -------- FUNCIONES - MENU - CRUD ----------#
+
+def crear():
+    miconexion = sqlite3.connect("Usuarios")
+    miCursor = miconexion.cursor()
+    try:
+        miCursor.execute("INSERT INTO DATOSUSUARIOS VALUES(NULL,?,?,?,?,?)", ( 
+            miNombre.get(), 
+            miPass.get(),
+            miApellido.get(),
+            miDireccion.get(),
+            textoComentario.get("1.0", END) 
+            )
+        )
+        miconexion.commit()
+        messagebox.showinfo("BBDD","Registro insertado con exito")
+    except :
+        messagebox.showwarning("!Error!","Ha ocurrido un error al cargar tus datos")
+    finally:
+        limpiarCampos()
+        miconexion.close()
         
+
+def leer():
+    miconexion = sqlite3.connect("Usuarios")
+    miCursor = miconexion.cursor()
+    try:
+        miCursor.execute("SELECT * FROM DATOSUSUARIOS WHERE ID= ?", miId.get())
+
+        elUsuario = miCursor.fetchall()
+
+        for usuario in elUsuario:
+            miId.set(usuario[0])
+            miNombre.set(usuario[1]), 
+            miPass.set(usuario[2]),
+            miApellido.set(usuario[3]),
+            miDireccion.set(usuario[4]),
+            textoComentario.insert(1.0,usuario[5])
+
+        miconexion.commit()
+    except:
+        messagebox.showwarning("!Error!","No se ha encontrado el usuario solicitado")
+        miId.set("")
+    finally:
+        miconexion.close()
+
+def actualizar():
+    miconexion = sqlite3.connect("Usuarios")
+    miCursor = miconexion.cursor()
+    try:
+        miCursor.execute("UPDATE DATOSUSUARIOS SET NOMBRE_USUARIO=?, PASSWORD=?,APELLIDO=?,DIRECCION=?, COMENTARIOS=? WHERE ID= ? ",(
+            miNombre.get(),
+            miPass.get(),
+            miApellido.get(),
+            miDireccion.get(),
+            textoComentario.get("1.0", END),
+            miId.get()
+        ))
+        miconexion.commit()
+        messagebox.showinfo("BBDD","Registro actualizado con exito")
+    except:
+        messagebox.showwarning("!Error!","No se ha podido actualizar la informacion de el usuario solicitado")
+    finally:
+        limpiarCampos()
+        miconexion.close()
+
+def borrar():
+    miconexion = sqlite3.connect("Usuarios")
+    miCursor = miconexion.cursor()
+    try:
+        miCursor.execute("DELETE FROM DATOSUSUARIOS WHERE ID=?",miId.get())
+        miconexion.commit()
+        messagebox.showinfo("BBDD","Registro borrado con exito")
+    except:
+        messagebox.showwarning("!Error!","No se ha podido eliminar el usuario solicitado")
+    finally:
+        limpiarCampos()
+        miconexion.close()  
+
 # -------- CONFIG 1 TKINTER ----------#
 root = Tk()
 
@@ -41,13 +133,13 @@ bbddMenu.add_command(label="Conectar", command=lambda:conexionBBDD())
 bbddMenu.add_command(label="Salir", command=lambda:salirAplicacion())
 
 borrarMenu = Menu(barraMenu, tearoff=0)
-borrarMenu.add_command(label="Borrar")
+borrarMenu.add_command(label="Borrar", command=lambda:limpiarCampos())
 
 crudborrarMenu = Menu(barraMenu, tearoff=0)
-crudborrarMenu.add_command(label="Crear")
-crudborrarMenu.add_command(label="Leer")
-crudborrarMenu.add_command(label="Actualizar")
-crudborrarMenu.add_command(label="Borrar")
+crudborrarMenu.add_command(label="Crear", command=lambda:crear())
+crudborrarMenu.add_command(label="Leer", command=lambda:leer())
+crudborrarMenu.add_command(label="Actualizar", command=lambda:actualizar())
+crudborrarMenu.add_command(label="Borrar", command=lambda:borrar())
 
 ayudaMenu = Menu(barraMenu, tearoff=0)
 ayudaMenu.add_command(label="Licencia")
@@ -87,21 +179,27 @@ comentariosLabel.grid(row=5, column=0, sticky="e")
 
 # -------- ENTRY - FRAME 1 ----------#
 
-cuadroId = Entry(miFrame)
+miId = StringVar()
+miNombre = StringVar()
+miApellido = StringVar()
+miPass = StringVar()
+miDireccion = StringVar()
+
+cuadroId = Entry(miFrame, textvariable=miId)
 cuadroId.grid(row=0, column=1, pady=5 , padx=5)
 
-cuadroNombre = Entry(miFrame)
+cuadroNombre = Entry(miFrame, textvariable=miNombre)
 cuadroNombre.grid(row=1, column=1, pady=5 , padx=5)
 cuadroNombre.config(fg="red",justify="left")
 
-cuadroPass = Entry(miFrame)
+cuadroPass = Entry(miFrame, textvariable=miPass)
 cuadroPass.grid(row=2, column=1, pady=5 , padx=5)
 cuadroPass.config(show="*")
 
-cuadroApellido = Entry(miFrame)
+cuadroApellido = Entry(miFrame, textvariable=miApellido)
 cuadroApellido.grid(row=3, column=1, pady=5 , padx=5)
 
-cuadroDireccion = Entry(miFrame)
+cuadroDireccion = Entry(miFrame, textvariable=miDireccion)
 cuadroDireccion.grid(row=4, column=1,  pady=5 , padx=5)
 
 textoComentario = Text(miFrame, width=16, height=5)
@@ -113,16 +211,16 @@ textoComentario.config(yscrollcommand=scrollVert.set)
 
 # -------- BUTTON -  FRAME 2----------#
 
-botonCrear = Button(miFrame2, text="Crear", command=())
+botonCrear = Button(miFrame2, text="Crear", command=lambda:crear())
 botonCrear.grid(row=1,column=0, sticky="nsew",  pady=5 , padx=5)
 
-botonLeer = Button(miFrame2, text="Leer", command=())
+botonLeer = Button(miFrame2, text="Leer", command=lambda:leer())
 botonLeer.grid(row=1,column=1, sticky="nsew",  pady=5 , padx=5)
 
-botonActualizar = Button(miFrame2, text="Actualizar", command=())
+botonActualizar = Button(miFrame2, text="Actualizar",  command=lambda:actualizar())
 botonActualizar.grid(row=1,column=2, sticky="nsew",  pady=5 , padx=5)
 
-botonBorrar = Button(miFrame2, text="Borrar", command=())
+botonBorrar = Button(miFrame2, text="Borrar", command=lambda:borrar())
 botonBorrar.grid(row=1,column=3, sticky="nsew",  pady=5 , padx=5)
 
 # -------- CONFIG 2 TKINTER ----------#
